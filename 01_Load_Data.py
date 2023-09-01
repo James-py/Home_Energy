d8edb4e276564677ad6e56b4f6a8477ead0fbd00
James-py <jcboak@gmail.com>
2023-09-01 05:32:49 -0700

    revising cleaning multipliers

--- 01_Load_Data.py
+++ 01_Load_Data.py
@@ -101,11 +101,29 @@ kW = data_WIP.pivot(index='Date_Time', columns='Circuit', values='kW')
 
 kW.index = pd.to_datetime(kW.index)
 
-# %%
+# %% Plot Original Data Line Graph
 
+if False:
+    fig, ax = plt.subplots(1, 1, tight_layout=True)
+    kW.drop(columns=[
+        'DHWHP_Spy','Main_MTU']
+        ).plot(ax=ax, colormap=cm.gist_rainbow)
+    
+    kW.Main_MTU.plot(color='k',linestyle='-', ax=ax)
+    ax.legend()
+    ax.set_ylabel('Power (kW)')
+    ax.set_title('original data lines')
+
+# %% Data Cleaning
+
+for col in kW.columns:
+    kW.loc[kW[col]<0, col] = np.nan
+    kW.loc[kW[col]>1000, col] = np.nan
+
+# %% Plot Original Data
 
 fig, ax = plt.subplots(1, 1, tight_layout=True)
-kW.drop(columns=[
+kW.drop(columns=['K_Plg_2', 'K_Plg_4',
     'DHWHP_Spy','Main_MTU']
     ).plot.area(ax=ax, colormap=cm.gist_rainbow)
 
@@ -115,16 +133,28 @@ ax.set_ylabel('Power (kW)')
 ax.set_title('original data')
 
 # %% Data Cleaning
-kW.loc[kW.DHWHP_Spy<0, 'DHWHP_Spy'] = np.nan
-kW.loc[:,'Gar_Dryer_Mod'] = kW.loc[:,'Gar_Dryer'] * 1.05
-kW.loc[:,'K_Oven_Mod'] = kW.loc[:,'K_Oven'] * 1.05
-kW.loc[:,'Garage_Mod'] = kW.loc[:,'Garage'] * 0.6
-kW.loc[:,'K_Fridge_Mod'] = kW.loc[:,'K_Fridge'] * 0.9
-kW.loc[:,'Living_Rm_Mod'] = kW.loc[:,'Living_Rm'] * 0.8
-kW.loc[:,'Bed_G_Off_Mod'] = kW.loc[:,'Bed_G_Off'] * 0.5
-kW.loc[:,'K_DishW_Mod'] = kW.loc[:,'K_DishW'] * 0.4
-kW.loc[:,'DHW_MTU_Mod'] = kW.loc[:,'DHW_MTU'] * 1.0
+
+kW.loc[:,'Main_MTU_Mod'] = kW.loc[:,'Main_MTU'] - 0.05
+kW.loc[kW.Main_MTU_Mod<0, 'Main_MTU_Mod'] = np.nan
+kW.loc[:,'Out_Plugs_Mod'] = kW.loc[:,'Out_Plugs'] #* 0.8
+kW.loc[:,'Gar_Dryer_Mod'] = kW.loc[:,'Gar_Dryer'] #* 1.05
+kW.loc[:,'K_Oven_Mod'] = kW.loc[:,'K_Oven'] #* 1.05
+kW.loc[:,'Garage_Mod'] = kW.loc[:,'Garage'] * 1
+kW.loc[:,'K_Fridge_Mod'] = kW.loc[:,'K_Fridge'] * 1
+kW.loc[:,'Living_Rm_Mod'] = kW.loc[:,'Living_Rm'] #* 0.5  # was 0.75 on ECC
+kW.loc[:,'Bed_G_Off_Mod'] = kW.loc[:,'Bed_G_Off'] #* 0.5  # was 0.75 on ECC
+kW.loc[:,'K_DishW_Mod'] = kW.loc[:,'K_DishW'] #* 0.4
+kW.loc[:,'Bed_Main_Mod'] = kW.loc[:,'Bed_Main'] #* 0.4
+kW.loc[:,'DHWHP_Spy_Mod'] = kW.loc[:,'DHWHP_Spy'] * 0.71 # was 0.71 on ECC (both channels)
 kW.sort_index(axis=1, inplace=True)
+
+"""
+NOTE: On Aug 25 at ~ 6PM I changed all the spyder multipliers on the ECC back to 1 or 2
+I also added a second spyder legg for the double-pole of the Oven and for the Dishwasher
+finished at ~6:26PM
+"""
+
+
 # %% 
 
 """
@@ -134,12 +164,29 @@ tab20
 """
 
 fig, ax = plt.subplots(1, 1, tight_layout=True)
-kW.drop(columns=[
-    'DHWHP_Spy','Main_MTU', 'Gar_Dryer', 'K_Oven', 'DHW_MTU',
-    'Garage', 'K_Fridge', 'Living_Rm', 'Bed_G_Off', 'K_DishW']
+kW.drop(columns=['K_Plg_2', 'K_Plg_4',
+    'DHWHP_Spy','DHWHP_Spy_Mod', 'Main_MTU', 'Main_MTU_Mod', 'Gar_Dryer', 'K_Oven', 'Out_Plugs',
+    'Garage', 'K_Fridge', 'Living_Rm', 'Bed_G_Off', 'K_DishW', 'Bed_Main']
     ).plot.area(ax=ax, colormap=cm.gist_rainbow)
 
-kW.Main_MTU.plot(color='k',linestyle='-', ax=ax)
+kW.Main_MTU_Mod.plot(color='k',linestyle='-', ax=ax)
 ax.legend()
 ax.set_ylabel('Power (kW)')
 ax.set_title('cleaned data')
+
+# %%
+
+fig, ax = plt.subplots(1, 1, tight_layout=True)
+kW.loc[:,['DHWHP_Spy_Mod','DHW_MTU']].plot(ax=ax,alpha=0.35)
+ax.legend()
+ax.set_ylabel('Power (kW)')
+ax.set_title('DHW HP')
+
+# %%
+
+fig, ax = plt.subplots(1, 1, tight_layout=True)
+kW.loc[:,['K_Plg_1Is']].plot(ax=ax,alpha=0.35)
+ax.legend()
+ax.set_ylabel('Power (kW)')
+ax.set_title('Kitchen Plugs')
+
