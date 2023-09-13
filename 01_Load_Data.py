9c6dafc47b6fc6d7c2824232fe11bc67befeecae
James-py <jcboak@gmail.com>
2023-09-13 07:55:48 -0700

    added missing spyder data

--- 01_Load_Data.py
+++ 01_Load_Data.py
@@ -130,23 +130,38 @@ ax.set_title('Original Power Data - Area')
 
 kW_mod = kW.copy()
 
-kW_mod.loc[:,'K_Plg_4Ts'] = kW.loc[:,'K_Plg_4Ts'] * 1.08
-kW_mod.loc[:,'Gar_Dryer'] = kW.loc[:,'Gar_Dryer'] * 1.08
-kW_mod.loc[:,'K_Oven'] = kW.loc[:,'K_Oven'] * 1.05
-kW_mod.loc[:,'Out_Plugs'] = kW.loc[:,'Out_Plugs'] * 0.55
-# kW_mod.loc[:,'K_Fridge'] = kW.loc[:,'K_Fridge'] * 1
-kW_mod.loc[:,'Living_Rm'] = kW.loc[:,'Living_Rm'] * 0.75  # was 0.75 on ECC
-kW_mod.loc[:,'Bed_G_Off'] = kW.loc[:,'Bed_G_Off'] * 0.85  # was 0.75 on ECC
+# kW_mod.loc[:,'K_Plg_1Is'] = kW.loc[:,'K_Plg_1Is'] * 0.95
+kW_mod.loc[:,'K_Plg_4Ts'] = kW.loc[:,'K_Plg_4Ts'] * 1.04
 kW_mod.loc[:,'K_DishW'] = kW.loc[:,'K_DishW'] * 0.75
-kW_mod.loc[:,'Bed_Main'] = kW.loc[:,'Bed_Main'] * 0.85
+kW_mod.loc[:,'Gar_Dryer'] = kW.loc[:,'Gar_Dryer'] * 1.04
+kW_mod.loc[:,'K_Oven'] = kW.loc[:,'K_Oven'] * 1.04
+kW_mod.loc[:,'Out_Plugs'] = kW.loc[:,'Out_Plugs'] * 0.55
+kW_mod.loc[:,'Garage'] = kW.loc[:,'Garage'] * 0.95
+kW_mod.loc[:,'K_Fridge'] = kW.loc[:,'K_Fridge'] * 0.95
+kW_mod.loc[:,'Living_Rm'] = kW.loc[:,'Living_Rm'] * 0.75  # was 0.75 on ECC
+kW_mod.loc[:,'Bed_G_Off'] = kW.loc[:,'Bed_G_Off'] * 0.75  # was 0.75 on ECC
+kW_mod.loc[:,'Bed_Main'] = kW.loc[:,'Bed_Main'] * 0.75
 kW_mod.loc[:,'DHWHP_Spy'] = kW.loc[:,'DHWHP_Spy'] * 0.71 # was 0.71 on ECC (both channels)
 kW_mod.sort_index(axis=1, inplace=True)
+
 
 """
 NOTE: On Aug 25 at ~ 6PM I changed all the spyder multipliers on the ECC back to 1 or 2
 I also added a second spyder legg for the double-pole of the Oven and for the Dishwasher
 finished at ~6:26PM
 """
+
+
+# Total Power Comparison Calcs
+# - uses spyder data for DHW
+# fill Main MTU data using spyder sum
+
+kW_tot_compare = pd.DataFrame(kW_mod['Main_MTU'])
+kW_tot_compare['Spy_Sum'] = kW_mod.drop(columns=['DHW_MTU','Main_MTU']).sum(axis=1)
+kW_tot_compare['Main_MTU'] = kW_tot_compare['Main_MTU'].fillna(kW_tot_compare.Spy_Sum)
+
+kW_mod['Spy_missing'] = kW_mod['Main_MTU'] - kW_tot_compare['Spy_Sum']
+kW_mod.loc[kW_mod['Spy_missing']<0, 'Spy_missing'] = 0
 
 
 # %% Cleaned Power Data Area Plot
@@ -167,7 +182,7 @@ ax.set_ylabel('Power (kW)')
 ax.set_title('Cleaned Power Data - Area')
 
 
-# %% DHW Spyder Vs MTU Comparison
+# %% DHW Spyder Vs MTU Comparison Plot
 
 fig, ax = plt.subplots(1, 1, tight_layout=True)
 kW_mod.loc[:,['DHWHP_Spy','DHW_MTU']].plot(ax=ax,alpha=0.35, x_compat=True)
@@ -175,14 +190,6 @@ ax.legend()
 ax.set_ylabel('Power (kW)')
 ax.set_title('DHW HP Spyder vs MTU')
 
-
-# %% Total Power Comparison 
-# - uses spyder data for DHW
-# fill Main MTU data using spyder sum
-
-kW_tot_compare = pd.DataFrame(kW_mod['Main_MTU'])
-kW_tot_compare['Spy_Sum'] = kW_mod.drop(columns=['DHW_MTU','Main_MTU']).sum(axis=1)
-kW_tot_compare['Main_MTU'] = kW_tot_compare['Main_MTU'].fillna(kW_tot_compare.Spy_Sum)
 
 # %% Plot Power Total Comparison
 
@@ -224,32 +231,61 @@ for n, f in BCH_data_files.items():
 BCH_data_raw = pd.concat(BCH_data_dict)
 BCH_data_WIP = BCH_data_raw.drop_duplicates(subset=['Interval Start Date/Time'])
 BCH_data_WIP['Date_Time'] = pd.to_datetime(BCH_data_WIP['Interval Start Date/Time'])
-BCH_data_WIP['Date_Time'] = BCH_data_WIP.Date_Time.dt.floor('Min')
+BCH_data_WIP.loc[:,'Date_Time'] = BCH_data_WIP.Date_Time.dt.floor('Min')
 
-# %% create df with only energy and set datetime index
+# %% create df with only BCH energy and set datetime index
 BCH_kWh = BCH_data_WIP.pivot(index='Date_Time', 
                              columns='Account Number', 
                              values='Net Consumption (kWh)')
 BCH_kWh.index = pd.to_datetime(BCH_kWh.index)
 
 
-#%% calculate hourly energy and combine TED and BCH data
+#%% calculate hourly energy 
 
-kWh = kW_tot_compare.resample('1H').mean()
-kWh['MTU_Spy_Diff'] = kWh.Main_MTU - kWh.Spy_Sum
-kWh.loc[kWh['MTU_Spy_Diff']<0, 'MTU_Spy_Diff'] = np.nan
-kWh['BCH'] = BCH_kWh[12014857]
-kWh['MTU_BCH_Diff'] = kWh.Main_MTU - kWh.BCH
+kWh = kW_mod.resample('1H').mean()
+
+# %% Plot Energy Area
+fig, ax = plt.subplots(1, 1, tight_layout=True)
+kWh.drop(columns=['DHW_MTU','Main_MTU']
+    ).plot.area(ax=ax, colormap=cm.gist_rainbow, x_compat=True)
+
+kWh.Main_MTU.plot(color='k',linestyle='-', ax=ax)
+ax.legend()
+ax.set_ylabel('Hourly Energy (kWh)')
+ax.set_title('Cleaned Energy Data - Area')
+
+
+# %% Total Energy Compare and combine TED and BCH data
+
+kWh_tot_compare = pd.DataFrame(kWh[['Main_MTU', 'DHWHP_Spy']])
+kWh_tot_compare['Spy_Sum'] = kWh.drop(columns=['DHW_MTU','Main_MTU']).sum(axis=1)
+kWh_tot_compare['Main_MTU'] = kWh['Main_MTU'].fillna(kWh_tot_compare.Spy_Sum)
+kWh_tot_compare['MTU_Spy_Diff'] = kWh_tot_compare.Main_MTU - kWh_tot_compare.Spy_Sum
+
+kWh_tot_compare.loc[kWh_tot_compare['MTU_Spy_Diff']<0, 'MTU_Spy_Diff'] = np.nan
+kWh_tot_compare['BCH'] = BCH_kWh[12014857]
+kWh_tot_compare['MTU_BCH_Diff'] = kWh_tot_compare.Main_MTU - kWh_tot_compare.BCH
 
 # %% Plot hourly Energy data
 
 fig, ax = plt.subplots(1, 1, tight_layout=True)
-kWh.plot(ax=ax,alpha=0.75, x_compat=True)
+kWh_tot_compare.drop(columns=['DHWHP_Spy']).plot(ax=ax,alpha=0.75, x_compat=True)
 ax.legend()
 ax.set_ylabel('Energy (kWh)')
 ax.set_title('Compare totals - Energy')
 ax.axhline(color='k')
 
-# %% print table
-print(kWh.resample('1d').sum())
+# %% Daily Energy Totals
+
+kWh_daily = kWh_tot_compare.resample('1d').sum()
+
+kWh_daily['MTU_Spy_Diff_pct'] = kWh_daily.MTU_Spy_Diff / kWh_daily.Spy_Sum *100
+kWh_daily['MTU_BCH_Diff_pct'] = kWh_daily.MTU_BCH_Diff / kWh_daily.BCH *100
+
+
+print("Average daily DHW HP Energy Consumption:", 
+      kWh_daily.DHWHP_Spy['2023-09-01':].mean(), "kWh")
+
+# print table
+print(kWh_daily)
 
