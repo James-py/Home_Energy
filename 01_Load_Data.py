cd0573dd353a045e949695c17c2a6192c97788b5
James-py <jcboak@gmail.com>
2023-09-22 13:31:14 -0700

    Filled spyder submeters when power consumption was low

--- 01_Load_Data.py
+++ 01_Load_Data.py
@@ -37,22 +37,23 @@ otherplots = False
 
 def simple_plot(df, caption, vertLine=1000, xlab='Date / Time', ylab='',
                 legend=False,):
-    fig, ax = plt.subplots(1, 1, tight_layout=True, num=caption)
+    fig, ax = plt.subplots(1, 1, tight_layout=True) #, num=caption)
     df.plot(ax=ax, marker='.', alpha=0.35, linestyle='None',legend=legend,
             x_compat=True)
     if vertLine != 1000:
         plt.axvline(vertLine)
     ax.set_xlabel(xlab)
     ax.set_ylabel(ylab)
+    ax.set_title(caption)
 
 
 def area_plot(df, caption, legend=False, ylab=''):
-    fig, ax = plt.subplots(1, 1, tight_layout=True, num=caption)
+    fig, ax = plt.subplots(1, 1, tight_layout=True) #, num=caption)
     df.plot.area(ax=ax, colormap=cm.gist_rainbow, x_compat=True, legend=legend)
     # marker='.', alpha=0.5,linestyle='None',
     ax.set_ylabel(ylab)
     ax.set_xlabel("Date / Time")
-    # ax.set_title(caption+'\n(n='+str(len(df.columns))+')')
+    ax.set_title(caption)
 
 
 # %% set up folder paths
@@ -118,12 +119,15 @@ for col in kW.columns:
     kW.loc[kW[col]<0, col] = np.nan
     kW.loc[kW[col]>1000, col] = np.nan
 
-# %% Data Cleaning - Drop duplicate Oven data
 
-kW.loc['2023-09-20 12:00':,'K_Oven'] = np.nan
+
+# %%
+
+
+# kW.loc['2023-09-20 22:00':,'K_Oven'] = np.nan
 # %% Plot Original Data Area Graph
 
-fig, ax = plt.subplots(1, 1, tight_layout=True)
+fig, ax = plt.subplots(1, 1)
 kW.drop(columns=['DHW_MTU','Main_MTU','Test_MTU']
     ).plot.area(ax=ax, colormap=cm.gist_rainbow, x_compat=True)
 
@@ -132,9 +136,14 @@ ax.legend()
 ax.set_ylabel('Power (kW)')
 ax.set_title('Original Power Data - Area')
 
-# %% Data Cleaning - Spyder Leg Calibration
+# %% Make a copy of the kW data for data cleaning
+# Drop duplicate Oven data
 
 kW_mod = kW.copy()
+kW_mod.drop(columns=['K_Oven1','K_Oven2'], inplace=True)
+kW_mod.drop(columns=['DHWHP_Sp1','DHWHP_Sp2'], inplace=True)
+
+# %% Data Cleaning - Spyder Leg Calibration
 
 # kW_mod.loc[:,'K_Plg_1Is'] = kW.loc[:,'K_Plg_1Is'] * 0.95
 kW_mod.loc[:,'K_Plg_4Ts'] = kW.loc[:,'K_Plg_4Ts'] * 1.04
@@ -159,7 +168,44 @@ finished at ~6:26PM
 """
 
 
-# Total Power Comparison Calcs
+# %% Data Cleaning - fill K_Oven
+
+# kW_mod.loc[:,'K_Oven'] = kW_mod.loc[:,'K_Oven'].fillna(kW.K_Oven1*2)
+# kW_mod.loc[:,'K_Oven'] = kW_mod.loc[:,'K_Oven'].fillna(kW.K_Oven2*2)
+
+kW_mod.loc[kW.K_Oven1>0,'K_Oven'] = kW.loc[
+           kW.K_Oven1>0,['K_Oven1','K_Oven2']].sum(axis=1) * 1.04
+# kW_mod.loc[kW.K_Oven2>0,'K_Oven'] = kW.loc[kW.K_Oven2>0,'K_Oven2'] * 2
+
+# %% Data Cleaning - fill Heat Pump
+
+kW_mod.loc[kW.DHWHP_Sp1>0,'DHWHP_Spy'] = kW.loc[
+           kW.DHWHP_Sp1>0,['DHWHP_Sp1','DHWHP_Sp2']].sum(axis=1) * 0.71
+
+
+# %% Fill missing Living Room Data
+# the 30 W appears to be the router that is always on
+
+kW_mod.loc[kW.Living_Rm<0.03,'Living_Rm'] = 0.03
+
+# %% Fill missing Bedroom Data
+# When nothing else is on, various standby and chargers are still a small load
+
+kW_mod.loc[kW[['Bed_Main','Bed_G_Off']].sum(axis=1)==0, 
+           ['Bed_Main','Bed_G_Off']] = 0.02
+
+# kW_mod.loc[kW.Living_Rm<0.03,'Living_Rm'] = 0.03
+
+# %% Fill missing Freezer Data
+# When freezer is on, the spyder seems to underestimate the load sometimes
+
+kW_mod.loc[kW.Freezer.between(0.001,0.03), 'Freezer'] = 0.05
+
+
+
+# kW_mod.loc[kW.Living_Rm<0.03,'Living_Rm'] = 0.03
+
+# %% Total Power Comparison Calcs
 # - uses spyder data for DHW
 # fill Main MTU data using spyder sum
 
@@ -171,7 +217,7 @@ kW_tot_compare['Main_MTU'] = kW_tot_compare['Main_MTU'].fillna(kW_tot_compare.Sp
 # kW_mod.loc[kW_mod['Spy_missing']<0, 'Spy_missing'] = 0
 
 
-# %% Cleaned Power Data Area Plot
+# %% Plot Cleaned Power Data Area
 
 """
 XKCD_COLORS
@@ -189,8 +235,8 @@ ax.set_ylabel('Power (kW)')
 ax.set_title('Cleaned Power Data - Area')
 
 
-# %% DHW Spyder Vs MTU Comparison Plot
 
+# %% Plot DHW Spyder Vs MTU Comparison
 fig, ax = plt.subplots(1, 1)
 kW_mod.loc[:,['DHWHP_Spy','DHW_MTU']].plot(ax=ax,alpha=0.35, x_compat=True)
 ax.legend()
@@ -237,15 +283,13 @@ for n, f in BCH_data_files.items():
 
 BCH_data_raw = pd.concat(BCH_data_dict)
 BCH_data_WIP = BCH_data_raw.drop_duplicates(subset=['Interval Start Date/Time'])
-BCH_data_WIP['Date_Time'] = pd.to_datetime(BCH_data_WIP['Interval Start Date/Time'])
-BCH_data_WIP.loc[:,'Date_Time'] = BCH_data_WIP.Date_Time.dt.floor('Min')
+BCH_data_WIP.loc[:,'Date_Time'] = pd.to_datetime(BCH_data_WIP.loc[:,'Interval Start Date/Time'])
+BCH_data_WIP.loc[:,'Date_Time'] = BCH_data_WIP.loc[:,'Date_Time'].dt.floor('Min')
 
 # %% create df with only BCH energy and set datetime index
 BCH_kWh = BCH_data_WIP.pivot(index='Date_Time', 
                              columns='Account Number', 
                              values='Net Consumption (kWh)')
-BCH_kWh.index = pd.to_datetime(BCH_kWh.index)
-
 
 #%% calculate hourly energy 
 
@@ -273,7 +317,7 @@ kWh_tot_compare['MTU_Spy_Diff'] = kWh_tot_compare.Main_MTU - kWh_tot_compare.Spy
 kWh_tot_compare['BCH'] = BCH_kWh[12014857]
 kWh_tot_compare['MTU_BCH_Diff'] = kWh_tot_compare.Main_MTU - kWh_tot_compare.BCH
 
-# %% Plot hourly Energy data
+# %% Plot Hourly Energy Total Comparison
 
 fig, ax = plt.subplots(1, 1)
 kWh_tot_compare.drop(columns=['DHWHP_Spy']).plot(ax=ax,alpha=0.75, x_compat=True)
@@ -299,5 +343,10 @@ print(kWh_daily)
     
 # %% One-off Plot
 
-simple_plot(kWh.filter(like='Is'), 'Island', ylab='hourly energy kWh',
+area_plot(kWh.filter(like='DHWHP'), 'Heat Pump', ylab='hourly energy kWh',
+          legend=True)
+
+# %% One-off Plot
+
+area_plot(kW_mod.filter(like='Oven'), 'Oven', ylab='power kW',
           legend=True)
