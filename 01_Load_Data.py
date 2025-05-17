2620fa7d1cfcf8ba50698284a003819fea8204dc
James-py <james.boak@gmail.com>
2025-05-17 11:30:32 -0700

    weather query

--- 01_Load_Data.py
+++ 01_Load_Data.py
@@ -9,7 +9,7 @@ terminal command to get data from Raspberry Pi
 rsync -a james@rpi.local:/home/james/Data/ ~/repo/Home_Energy/Data_Raw
 
 Use wget in Terminal to get Environment Canada Weather Data
-for year in `seq 2023 2024`;do for month in `seq 1 12`;do wget --content-disposition "https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=52941&Year=${year}&Month=${month}&Day=14&timeframe=1&submit= Download+Data" ;done;done
+for year in `seq 2024 2025`;do for month in `seq 1 12`;do wget --content-disposition "https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=52941&Year=${year}&Month=${month}&Day=14&timeframe=1&submit= Download+Data" ;done;done
 
 """
 
