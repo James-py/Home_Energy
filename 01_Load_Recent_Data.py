c8f950d925ba505fc39906e48ac9d4d4d4264fd9
James Boak <james@carbonsight.com>
2025-11-18 16:45:53 -0800

    new script for analyzing recent data

--- 01_Load_Recent_Data.py
+++ 01_Load_Recent_Data.py
@@ -4,7 +4,6 @@ Created on Thu Aug 24 05:36:44 2023
 
 magic command for plots:
 %matplotlib
-%matplotlib ipympl
 
 terminal command to get data from Raspberry Pi
 rsync -a james@10.0.0.58:/home/james/Data/ ~/repo/Home_Energy/Data_Raw
@@ -18,14 +17,15 @@ for month in `seq 1 12`;do wget --content-disposition "https://climate.weather.g
 
 # %% Import Libraries
 
-from pathlib import Path
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import datetime
-import seaborn as sns
+from IPython.core import display
 from matplotlib.colors import ListedColormap
 from mpl_axes_aligner import align
+from pathlib import Path
+import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
 
 
 # %% get today's date
@@ -107,6 +107,7 @@ def dots_plot(df, caption, vertLine=1000, xlab="Date / Time", ylab="", legend=Fa
     ax.set_xlabel(xlab)
     ax.set_ylabel(ylab)
     ax.set_title(caption)
+    plt.show()
 
 
 def area_plot(df, caption, drop_list=[], main_line=False, legend=False, ylab="", start_date=one_week_ago):
@@ -118,6 +119,7 @@ def area_plot(df, caption, drop_list=[], main_line=False, legend=False, ylab="",
     ax.set_ylabel(ylab)
     ax.set_xlabel("Date / Time")
     ax.set_title(caption)
+    plt.show()
 
 
 def lines_plot(
@@ -140,6 +142,7 @@ def lines_plot(
         ax2.set_ylabel("Temperature (°C)", color=colour2)
         ax2.tick_params(axis="y", labelcolor=colour2)
         align.yaxes(ax, 0, ax2, 0, 0.2)
+    plt.show()
 
 
 # plt.close('all')
@@ -155,10 +158,15 @@ CWD = Path(__file__).parent.resolve()
 print("\nCWD Folder")
 print(CWD)
 
-# raw data folder path object
-RAWDataPath = CWD.joinpath("Data_Raw")
-print("\nSource Data (Raw) Folder")
-print(RAWDataPath)
+# # raw data folder path object
+# RAWDataPath = CWD.joinpath("Data_Raw")
+# print("\nSource Data (Raw) Folder")
+# print(RAWDataPath)
+
+# recent data folder path object
+RecentDataPath = CWD.joinpath("Data_recent")
+print("\nSource Data (Recent) Folder")
+print(RecentDataPath)
 
 # BC Hydro data folder path object
 BCH_Path = CWD.joinpath("Data_BC_Hydro")
@@ -179,7 +187,7 @@ def get_filepaths(f_path, f_pattern):
     return file_dict
 
 
-TED_files = get_filepaths(RAWDataPath, "*.csv")
+TED_files = get_filepaths(RecentDataPath, "*.csv")
 Weather_files = get_filepaths(Weather_Path, "*.csv")
 
 # %%
@@ -234,70 +242,45 @@ for col in kW.columns:
     kW.loc[kW[col] < 0, col] = np.nan
     kW.loc[kW[col] > 12, col] = np.nan
 
-# %% Make a copy of the kW data for data cleaning
-# Drop duplicate Oven data
+# %% Make working copy of the kW data for data cleaning
 
-kW_mod = kW.copy()
-kW_mod.drop(columns=["K_Oven1", "K_Oven2"], inplace=True)
-kW_mod.drop(columns=["DHWHP_Sp1", "DHWHP_Sp2"], inplace=True)
-kW_mod.drop(columns=["K_DishW_1", "K_DishW_2"], inplace=True)
-kW_mod.drop(columns=["Gar_Dry1", "Gar_Dry2"], inplace=True)
+kW_mod = pd.DataFrame()
+
+# %%
+# Combine double pole dryer data
+
+kW_mod["Gar_Dryer"] = kW.loc[:, ["Gar_Dry1", "Gar_Dry2"]].sum(axis=1) * 1.04
 
 # %% Data Cleaning - Spyder Leg Calibration
 
 calibration_dict = {
-    "K_Plg_4Ts": 1.04,
-    "K_DishW": 0.55,
-    "Gar_Dryer": 1,
-    "K_Oven": 1.1,
-    "Out_Plugs": 0.7,
-    "Garage": 0.8,
-    "Freezer": 0.85,
-    "K_Fridge": 0.85,
-    "Living_Rm": 0.6,
     "Bed_G_Off": 0.6,
     "Bed_Main": 0.60,
     "DHWHP_Spy": 0.7,
-    "K_Plg_2MW": 0.75,
+    "Freezer": 0.85,
+    "Garage": 0.8,
     "Heat_Beds": 1.0,
     "Heat_LvRm": 1.0,
+    "K_DishW": 0.55,
+    "K_Fridge": 1.0,  # 0.85,
+    "K_Oven": 1.1,
+    "K_Plg_2MW": 0.75,
+    "K_Plg_4Ts": 1.04,
+    "K_Plg_5LS": 1.0,
+    "K_Plg_IsL": 1.0,
+    "K_Plg_IsU": 1.0,
+    "Living_Rm": 0.6,
+    "Out_Plugs": 0.7,
+    "Main_MTU": 1.0,
+    "Test_MTU": 1.0,
 }
 
+# %%
+# Copy desired data from raw and apply the calibration multiplier
 for circuit, multiplier in calibration_dict.items():
-    kW_mod.loc[:, circuit] = kW.loc[:, circuit] * multiplier
+    kW_mod[circuit] = kW.filter(like=circuit) * multiplier
 
 kW_mod.sort_index(axis=1, inplace=True)
-
-
-"""
-NOTE: On Aug 25 at ~ 6PM I changed all the spyder multipliers on the ECC back to 1 or 2
-I also added a second spyder legg for the double-pole of the Oven and for the Dishwasher
-finished at ~6:26PM
-"""
-
-# %% Fix Heating OLD
-
-
-def fix_heating(df_mod, df, circuit, cutoff, offset, multiplier1, spike):
-    df_mod.loc[:, circuit] = df.loc[:, circuit]
-    # # when signal is > cutoff
-    rows = df[circuit] > cutoff
-    df_mod.loc[rows, circuit] = df.loc[rows, circuit] + offset + (df.loc[rows, circuit] - cutoff) * multiplier1
-    # when signal is <= cutoff and >= offset
-    # with a 0.05 W deadband
-    rows = (df[circuit] <= (cutoff - 0.05)) & (df[circuit] >= offset)
-    df_mod.loc[rows, circuit] = df.loc[rows, circuit] - offset
-    # when signal is < offset
-    df_mod.loc[(df[circuit] < offset), circuit] = 0
-
-    # when signal is > spike
-    rows = df[circuit] > spike
-    df_mod.loc[rows, circuit] = spike - 0.5
-
-
-# fix_heating(kW_mod, kW, 'Heat_LvRm', 0.75, 0.15, 0.25, 3.0)
-
-# fix_heating(kW_mod, kW, 'Heat_Beds', 0.9, 0.15, 0.25, 3.0)
 
 # %% Fix Heating V2
 
@@ -320,112 +303,6 @@ def fix_heating_v2(df_mod, df, circuit, dc_offset, linear_scale, cutoff):
 fix_heating_v2(kW_mod, kW, "Heat_LvRm", 0.15, 1.2, 2.6)
 
 fix_heating_v2(kW_mod, kW, "Heat_Beds", 0.15, 1.4, 3.0)
-
-# %% Bedroom Heat and Test MTU kW
-if False:
-    lines_plot(
-        kW_mod.loc[:, ["Heat_Beds", "Test_MTU"]],  # .resample('1d').sum(),
-        weather_df,  # .resample('1d').mean(),
-        "Bedroom Heat mod - power",
-        ylab="kW",
-        start_date="2024-09-29",
-        legend=True,
-        vs_temp=False,
-    )
-
-    # plt.close(fig='all')
-    lines_plot(
-        kW.loc[:, ["Heat_Beds", "Test_MTU"]],  # .resample('1d').sum(),
-        weather_df,  # .resample('1d').mean(),
-        "Bedroom Heat Original - power",
-        ylab="kW",
-        start_date="2024-09-29",
-        legend=True,
-        vs_temp=False,
-    )
-
-# %%
-# TODO Fix this
-# living room thermostat died Nov 23, 2023, and we installed the newer programmable thermostat
-# We changed the bedroom thermostats around the same time too
-# fix_heating(kW_mod.loc['2023-11-24':,:], kW.loc['2023-11-24':,:], 'Heat_Beds', 0.45, 0.15, 0.25, 3.0)
-
-# %% Heat Fix Plots for testing
-if False:
-    # Bed Rooms
-    lines_plot(
-        kW.loc[:"2023-10-28", ["Heat_Beds", "Test_MTU"]],
-        weather_df,
-        "Heat_Beds kW Original Oct 10 to 28",
-        ylab="kW",
-        start_date="2023-10-10",
-        legend=True,
-        vs_temp=False,
-    )
-
-    lines_plot(
-        kW_mod.loc[:"2023-10-28", ["Heat_Beds", "Test_MTU"]],
-        weather_df,
-        "Heat_Beds kW_mod Oct 10 to 28",
-        ylab="kW",
-        start_date="2023-10-10",
-        legend=True,
-        vs_temp=False,
-    )
-
-    # %%Living Room
-    lines_plot(
-        kW.loc[:"2023-11-13", ["Heat_LvRm", "Test_MTU"]],
-        weather_df,
-        "Heat Living Room kW Original Oct 28",
-        ylab="kW",
-        start_date="2023-10-28",
-        legend=True,
-        vs_temp=False,
-    )
-
-    # %%
-
-    lines_plot(
-        kW_mod.loc[:"2023-11-13", ["Heat_LvRm", "Test_MTU"]],
-        weather_df,
-        "Heat Living Room kW_mod Oct 28",
-        ylab="kW",
-        start_date="2023-10-28",
-        legend=True,
-        vs_temp=False,
-    )
-
-
-# %% Data Cleaning - double pole circuits
-"""
-some double pole circuits are symmetrical, so I don't need to use two spyder CTs
-I started with two, and then changed to using just one CT
-That meant changing the multiplier on the MTU and creating extra channels of
-data while I was checking things
-created some irregular data
-This section fixes that
-- combines the extra data channels back into one and drops the extra channels
-
-"""
-
-
-def fix_double_pole(df_mod, df, circuit, c1, c2, multiplier):
-    df_mod.loc[df[c1] > 0, circuit] = df.loc[df[c1] > 0, [c1, c2]].sum(axis=1) * multiplier
-    df_mod[circuit] = df_mod[circuit].fillna(0)
-
-
-# K_Oven
-fix_double_pole(kW_mod, kW, "K_Oven", "K_Oven1", "K_Oven2", 1.04)
-
-# DHW Heat Pump
-fix_double_pole(kW_mod, kW, "DHWHP_Spy", "DHWHP_Sp1", "DHWHP_Sp2", 0.71)
-
-# Dishwasher
-fix_double_pole(kW_mod, kW, "K_DishW", "K_DishW_2", "K_DishW_1", 0.55)
-
-# Laundry Dryer
-fix_double_pole(kW_mod, kW, "Gar_Dryer", "Gar_Dry1", "Gar_Dry2", 1.04)
 
 
 # %% Fill Missing Power Load
@@ -453,50 +330,27 @@ kW_mod.loc[kW.Freezer.between(0.01, 0.03), "Freezer"] = 0.05
 # fill Main MTU data using spyder sum
 
 kW_tot_compare = pd.DataFrame(kW_mod["Main_MTU"])
-kW_tot_compare["Spy_Sum"] = kW_mod.drop(columns=["DHW_MTU", "Main_MTU", "Test_MTU"]).sum(axis=1)
+kW_tot_compare["Spy_Sum"] = kW_mod.drop(columns=["Main_MTU", "Test_MTU"]).sum(axis=1)
 kW_tot_compare["Main_MTU"] = kW_tot_compare["Main_MTU"].fillna(kW_tot_compare.Spy_Sum)
 
 # %% import BC Hydro data
 
 BCH_data_files = {}
-
 for f in BCH_Path.glob("*.csv"):
     BCH_data_files[f.stem] = f
 
-
-# BCH_col_names = [
-#     'Circuit',
-#     'Date_Time',
-#     'kW',
-#     'cost',
-#     'Voltage',
-#     'PF',
-#     ]
 BCH_data_dict = {}
-
 for n, f in BCH_data_files.items():
     BCH_data_dict[n] = pd.read_csv(f)
 
-
-# %%
-
 for n, df in BCH_data_dict.items():
-    print(n)
     df["Date_Time"] = pd.to_datetime(df["Interval Start Date/Time"], yearfirst=True).dt.floor("Min")
     if df["Account Number"].dtype == "O":
         df["Account Number"] = pd.to_numeric(df["Account Number"].str.removeprefix("'"))
-    display(df.head(3))
-    print("\n")
+
 # %%
 # Combine BCH data into one dataframe and drop duplicates
 
-
-# df[['Account Number', 'Interval Start Date/Time', 'Net Consumption (kWh)',
-#        'Demand (kW)', 'Power Factor (%)']]
-
-# BCH_data_raw = pd.concat(BCH_data_dict)
-
-# %%
 BCH_data_WIP = (
     pd.concat(BCH_data_dict).reset_index().drop_duplicates(subset=["Account Number", "Date_Time"], keep="first")
 )
@@ -512,7 +366,7 @@ kWh = kW_mod.resample("1h").mean()
 # %% Total Energy Compare and combine TED and BCH data
 
 kWh_tot_compare = pd.DataFrame(kWh[["Main_MTU", "DHWHP_Spy"]])
-kWh_tot_compare["Spy_Sum"] = kWh.drop(columns=["DHW_MTU", "Main_MTU", "Test_MTU"]).sum(axis=1)
+kWh_tot_compare["Spy_Sum"] = kWh.drop(columns=["Main_MTU", "Test_MTU"]).sum(axis=1)
 kWh_tot_compare["Main_MTU"] = kWh["Main_MTU"].fillna(kWh_tot_compare.Spy_Sum)
 kWh_tot_compare["MTU_Spy_Diff"] = kWh_tot_compare.Main_MTU - kWh_tot_compare.Spy_Sum
 
@@ -530,8 +384,7 @@ print(
 )
 
 # %% print table
-display(kWh_daily.iloc[-10:, :].map("{:,.2f}".format))
-
+kWh_daily.iloc[-10:, :].map("{:,.2f}".format)
 
 # %%
 # calculate daily average temperature
@@ -583,56 +436,47 @@ ax.set_xticklabels(plot_days_df[start_date:].index.strftime("%Y-%m-%d"))
 
 # kWh_tot_compare_C = kWh_tot_compare.join(weather_df['Temp (°C)'])
 
-lines_plot(
-    kWh_tot_compare,
-    weather_df.loc[:, ["Temp (°C)", "Rel Hum (%)"]].resample("1d").mean(),
-    "MTU and Spyder Total Energy Comparison",
-    drop_list=["DHWHP_Spy", "BCH", "MTU_BCH_Diff"],
-    legend=True,
-    ylab="Hourly Energy (kWh)",
-    start_date=start_date,
-    vs_temp=True,
-)
+# lines_plot(
+#     kWh_tot_compare,
+#     weather_df.loc[:, ["Temp (°C)", "Rel Hum (%)"]].resample("1d").mean(),
+#     "MTU and Spyder Total Energy Comparison",
+#     drop_list=["DHWHP_Spy", "BCH", "MTU_BCH_Diff"],
+#     legend=True,
+#     ylab="Hourly Energy (kWh)",
+#     start_date=start_date,
+#     vs_temp=True,
+# )
 
-lines_plot(
-    kWh_tot_compare,
-    weather_df.loc[:, ["Temp (°C)", "Rel Hum (%)"]].resample("1d").mean(),
-    "MTU and BCH Total Energy Comparison",
-    drop_list=["DHWHP_Spy", "Spy_Sum", "MTU_Spy_Diff"],
-    legend=True,
-    ylab="Hourly Energy (kWh)",
-    start_date=start_date,
-    vs_temp=True,
-)
-
-
-# %% Plot Cleaned Power Data Area All
-
-area_plot(
-    kW_mod,
-    "Power Data Mod - Area",
-    main_line=True,
-    drop_list=["DHW_MTU", "Main_MTU", "Test_MTU"],
-    legend=True,
-    ylab="Power (kW)",
-    start_date=start_date,
-)
-
-# %% Plot Power Total Comparison
-if False:
-    lines_plot(kW_tot_compare, "MTU and Spyder Total Power Comparison", drop_list=[], legend=True, ylab="Power (kW)")
+# lines_plot(
+#     kWh_tot_compare,
+#     weather_df.loc[:, ["Temp (°C)", "Rel Hum (%)"]].resample("1d").mean(),
+#     "MTU and BCH Total Energy Comparison",
+#     drop_list=["DHWHP_Spy", "Spy_Sum", "MTU_Spy_Diff"],
+#     legend=True,
+#     ylab="Hourly Energy (kWh)",
+#     start_date=start_date,
+#     vs_temp=True,
+# )
 
 
-# %% Plot Original Data Area Graph
-if False:
-    area_plot(
-        kW,
-        "Original Power Data - Area",
-        main_line=True,
-        drop_list=["DHW_MTU", "Main_MTU", "Test_MTU"],
-        legend=True,
-        ylab="Power (kW)",
-    )
+# %%
+# change matplotlib backend
+
+# plt.switch_backend("tkagg")
+plt.switch_backend("ipympl")
+
+
+# %%
+# Plot Cleaned Power Data Area All
+# area_plot(
+#     kW_mod,
+#     "Power Data Mod - Area",
+#     main_line=True,
+#     drop_list=["Main_MTU", "Test_MTU"],
+#     legend=True,
+#     ylab="Power (kW)",
+#     start_date=start_date,
+# )
 
 
 # %% Plot Hourly Energy Area all channels
@@ -642,191 +486,83 @@ area_plot(
     kWh,
     "Hourly Energy - Area",
     main_line=True,
-    drop_list=["DHW_MTU", "Main_MTU", "Test_MTU"],
+    drop_list=["Main_MTU", "Test_MTU"],
     legend=True,
     ylab="Hourly Energy (kWh)",
     start_date=start_date,
 )
 
 
-# %% Bed Rooms
+# plt.close(fig='all')
 
-lines_plot(
-    kW.loc["2023-10-10 10:00":"2023-10-28 13:20", ["Heat_Beds", "Test_MTU"]],
-    weather_df.loc["2023-10-10 10:00":"2023-10-28 13:20", :],
-    "Heat_Beds kW Oct 10 to 28",
-    start_date="2023-10-10",
+# %%
+# Outdoor plugs Circuit Energy Lines
+# lines_plot(
+#     kWh.loc[:, ["Out_Plugs"]],  # .resample('1d').sum(),
+#     weather_df,  # .resample('1d').mean(),
+#     "Outdoor Plugs Hourly Energy",
+#     ylab="kWh",
+#     start_date="2025-09-01",
+#     legend=True,
+#     vs_temp=False,
+# )
+
+# %%
+# Outdoor plugs Circuit Power
+area_plot(
+    kW.loc[:, ["Out_Plugs"]],
+    "Outdoor Plugs Power - Raw",
     ylab="kW",
-    legend=True,
+    start_date=four_weeks_ago,
 )
 
-lines_plot(
-    kW_mod.loc["2023-10-10 10:00":"2023-10-28 13:20", ["Heat_Beds", "Test_MTU"]],
-    weather_df.loc["2023-10-10 10:00":"2023-10-28 13:20", :],
-    "Heat_Beds kW_mod Oct 10 to 28",
-    start_date="2023-10-10",
-    ylab="kW",
-    legend=True,
-)
-
-# %% Living Room
-
-lines_plot(
-    kW.loc["2023-10-28 13:30":"2024-03-09 8:32", ["Heat_LvRm", "Test_MTU"]],
-    weather_df,
-    "Heat Living Room kW Oct 28",
-    ylab="kW",
-    start_date="2023-10-28",
-    legend=True,
-)
-
-lines_plot(
-    kW_mod.loc["2023-10-28 13:30":"2024-03-09 8:32", ["Heat_LvRm", "Test_MTU"]],
-    weather_df,
-    "Heat Living Room kW_mod Oct 28",
-    ylab="kW",
-    start_date="2023-10-28",
-    legend=True,
-)
-
-# %% DHW Spyder vs Test MTU kW
-
-MTU_change_date = "2024-08-03 10:50"
-# corrected the DHW MTU polarity at 2024-03-12 8:04
-
-lines_plot(
-    kW_mod.loc[:MTU_change_date, ["DHWHP_Spy", "Test_MTU"]],
-    weather_df.loc[:MTU_change_date, :],
-    "DHW spy vs test MTU - Power",
-    ylab="kW",
-    start_date="2024-03-12",
-    legend=True,
-    vs_temp=True,
-)
-
-# %% DHW Spyder vs Test MTU kWh
-
-
-lines_plot(
-    kWh.loc[:MTU_change_date, ["DHWHP_Spy", "Test_MTU"]],  # .resample('1d').sum(),
-    weather_df.loc[:MTU_change_date, ["Temp (°C)", "Rel Hum (%)"]],  # .resample('1d').mean(),
-    "DHW spy vs test MTU - hourly energy",
+# %%
+# Outdoor plugs Circuit Energy Area
+area_plot(
+    kWh.loc[:, ["Out_Plugs"]],
+    "Outdoor Plugs Hourly Energy",
     ylab="kWh",
-    start_date="2024-03-12",
-    legend=True,
-    vs_temp=True,
+    start_date=four_weeks_ago,
 )
 
-
-# %% Bedroom Heat vs Test MTU kW
-# .loc[:,['Heat_Beds','Test_MTU']]
-#  weather_df.loc[:,['Temp (°C)','Rel Hum (%)']]
-
+# %% Fridge
 lines_plot(
-    kWh.loc[:, ["Heat_Beds", "Test_MTU"]],
+    kW.loc[:, ["K_Fridge"]],
     weather_df,
-    "Bedroom Heat Mod vs Test MTU - hourly energy",
-    ylab="kWh",
-    start_date=MTU_change_date,
-    legend=True,
-    vs_temp=True,
-)
-
-# plt.close(fig='all')
-
-# %% Bedroom Heat and Test MTU kW
-
-lines_plot(
-    kW_mod.loc[:, ["Heat_Beds", "Test_MTU"]],  # .resample('1d').sum(),
-    weather_df,  # .resample('1d').mean(),
-    "Bedroom Heat Mod 0.15 offset - power",
-    ylab="kW",
-    start_date=MTU_change_date,
-    legend=True,
-    vs_temp=True,
-)
-
-# plt.close(fig='all')
-
-# %% Bedroom Heat and Test MTU kW
-
-lines_plot(
-    kW.loc[:, ["Heat_Beds", "Test_MTU"]],  # .resample('1d').sum(),
-    weather_df,  # .resample('1d').mean(),
-    "Bedroom Heat Original - power",
-    ylab="kW",
-    start_date=MTU_change_date,
-    legend=True,
-    vs_temp=True,
-)
-
-# plt.close(fig='all')
-
-# %% Use Test MTU for Freezer for the week it was on that circuit
-
-# kW_mod.loc['2023-09-30 12:00':'2023-10-10 09:50','Freezer'] = kW_mod.loc[
-#     '2023-09-30 12:00':'2023-10-10 09:50','Test_MTU']
-
-# %% One-off lines Plot
-lines_plot(
-    kW_mod.loc[:, ["K_Fridge"]],  # .resample('1d').sum(),
-    weather_df,  # .resample('1d').mean(),
-    "Fridge hourly Power - Mod",
+    "Fridge Power",
     ylab="kW",
     start_date=four_weeks_ago,
     legend=True,
     vs_temp=False,
 )
 
-# %% One-off lines Plot
+# %%
 area_plot(
     kWh.loc[:, ["K_Fridge"]],  # .resample('1d').sum(),
-    "Fridge hourly energy - Mod",
+    "Fridge hourly energy",
     ylab="kWh",
     start_date=four_weeks_ago,
 )
 
-
-# lines_plot(kW.filter(like='DishW').resample('1H').mean(), 'Dishwasher', ylab='hourly energy kWh',
-#           legend=True)
-
-# lines_plot(kW[['Freezer','Test_MTU']], 'Freezer original power', ylab='kW',
-#           legend=True)
-
-# lines_plot(kW, 'original spyder data - lines',
-#            drop_list=['Main_MTU', 'DHW_MTU','Test_MTU'],
-#            legend=True, ylab='Power (kW)')
-
-# %% One-off area Plot
-
-# area_plot(kW_mod.filter(like='Oven'), 'Oven', ylab='power kW',
-#           legend=True)
-
-# %% One-off dots Plot
-
-# dots_plot(kW_mod[['Living_Rm','Test_MTU']], 'Living Room Mod', ylab='power kW',
-#           legend=True)
-
-# dots_plot(kW_mod[['Bed_Main','Test_MTU']], 'Bed Room Mod', ylab='power kW',
-#           legend=True)
-
-# dots_plot(kW_mod[['K_DishW','Test_MTU']], 'Dishwasher', ylab='power kW',
-#           legend=True)
-
-# dots_plot(kW_mod[['Freezer','Test_MTU']], 'Freezer Mod', ylab='power kW',
-#           legend=True)
+# %%
+area_plot(
+    kW.loc[:, ["K_Fridge"]],  # .resample('1d').sum(),
+    "Fridge power - Raw",
+    ylab="kW",
+    start_date=four_weeks_ago,
+)
 
 # %%
 # Hourly Total Bar Graph
 
 fig, ax = plt.subplots(layout="constrained")
 
-fridge_day = kWh.loc["2025-09-01":today, ["K_Fridge"]].resample("1d").sum()
+fridge_day = kWh.loc["2025-11-01":today, ["K_Fridge"]].resample("1d").sum()
 
 fridge_day.plot.bar(legend=False, ax=ax)
 
 ax.axhline(color="k")
-ax.set_ylabel("Hourly Energy Consumption (kWh)")
+ax.set_ylabel("Daily Energy Consumption (kWh)")
 ax.set_xlabel("Date")
 ax.set_xticklabels(ax.get_xticks(), rotation=90)
 ax.set_xticklabels(fridge_day.index.strftime("%Y-%m-%d %H:%M"))
